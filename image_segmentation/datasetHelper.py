f09cec1093032318b22ad8b75f2848f0e5b5ca9a
Karl Sousa <karlvandesman@gmail.com>
2019-11-10 13:58:38 -0300

    remove drop columns

--- image_segmentation/datasetHelper.py
+++ image_segmentation/datasetHelper.py
@@ -3,8 +3,8 @@ import pandas as pd
 class Dataset:
 
     def __init__(self):
-        self.train_dataset = pd.read_csv('./data/segmentation.data', skiprows=2).drop(['REGION-PIXEL-COUNT', 'SHORT-LINE-DENSITY-5', 'SHORT-LINE-DENSITY-2'], axis=1);
-        self.test_dataset = pd.read_csv('./data/segmentation.test', skiprows=2).drop(['REGION-PIXEL-COUNT', 'SHORT-LINE-DENSITY-5', 'SHORT-LINE-DENSITY-2'], axis=1);
+        self.train_dataset = pd.read_csv('./data/segmentation.data', skiprows=2)
+        self.test_dataset = pd.read_csv('./data/segmentation.test', skiprows=2)
 
     def split_views(self, dataset_):
         return dataset_[:,0:6], dataset_[:,6:];
