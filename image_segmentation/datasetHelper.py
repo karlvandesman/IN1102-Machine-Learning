5a83086ff5457ffad68b9daa37e0c3de2f6004ea
Karl Sousa <karlvandesman@gmail.com>
2019-11-12 12:14:34 -0300

    fix split views

--- image_segmentation/datasetHelper.py
+++ image_segmentation/datasetHelper.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
+pd.set_option('display.max_colwidth', 40)
+
 class Dataset:
 
     def __init__(self):
-        self.train_dataset = pd.read_csv('./data/segmentation.data', skiprows=2)
-        self.test_dataset = pd.read_csv('./data/segmentation.test', skiprows=2)
-
+        self.train_dataset = \
+        pd.read_csv('./data/segmentation.data', 
+                    skiprows=2).drop(['REGION-PIXEL-COUNT'], axis=1)
+        
+        self.test_dataset = \
+        pd.read_csv('./data/segmentation.test', 
+                    skiprows=2).drop(['REGION-PIXEL-COUNT'], axis=1)
+        
     def split_views(self, dataset_):
-        return dataset_[:,0:6], dataset_[:,6:];
+        col_shift = 9 - (19 - self.train_dataset.shape[1])
+        
+        return dataset_[:, 0:col_shift], dataset_[:, col_shift:];
 
     def getLabels(self, dataset_):
         return dataset_.index;
